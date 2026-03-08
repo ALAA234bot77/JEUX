36b4061eceba41f0b22fb2cc8ed92af33d381c69
me-it-s-me <yann.kierkegaard@efrei.net>
2026-03-08 18:52:29 +0100

    création d'une fenetre de jeux vide

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import pygame
 
+
+pygame.init()
 """This is the main program"""
+pygame.display.set_caption("game")
+pygame.display.set_mode((1080,720))
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
