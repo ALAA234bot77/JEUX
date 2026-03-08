b8717d8c2c5e951aa532247bfad43e5c20b352ef
Emi-Marin <emeline.marin@efrei.net>
2026-03-08 22:52:00 +0100

    crétion du joueur

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
 import pygame
-
-
-pygame.init()
 """This is the main program"""
-pygame.display.set_caption("game")
-pygame.display.set_mode((1080,720))
+pygame.init()
 
+#create a class to represent our game
+class Game:
+
+    def __init__(self):
+        self.player = Player()
+
+
+# create a class to represent the player
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.health = 3
+        self.max_health = 3
+        self.image = pygame.image.load('asset/perso_67.webp')
+        self.rect = self.image.get_rect()
+        self.rect.x = 300
+        self.rect.y =300
+
+
+pygame.display.set_caption("Trash Cat 67")
+screen = pygame.display.set_mode((1080,720))
+
+# import the background
+background=pygame.image.load('asset/bg_brouillon.jpg')
+
+game = Game()
 running = True
 
 while running:
+
+    #appliquer la fenetre de jeu
+    screen.blit(background,(200,200))
+
+    #appliquer l'image du joueur
+    screen.blit(game.player.image, game.player.rect)
+
+    pygame.display.flip()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
