ecaea47520ecdbf392ed81b8c064971806ad8f99
kavi <sonikavani5@gmail.com>
2026-03-10 13:23:11 +0100

    change the size of the background to size of screen

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ clock.tick(60)
 #generer notre ecran
 pygame.display.set_caption("Trash Cat 67")
 screen = pygame.display.set_mode((1080,720))
+background_img=pygame.image.load('asset/bg_brouillon.jpg')
+background= pygame.transform.scale(background_img,(1080,720)) #change the size of the background to size of screen
 
 # import the background
-background=pygame.image.load('asset/bg_brouillon.jpg')
+
 #charger le jeux
 game = Game()
 running = True
@@ -21,15 +23,15 @@ running = True
 while running:
 
     #appliquer la fenetre de jeu
-    screen.blit(background,(200,200))
+    screen.blit(background,(0,0))
 
     #appliquer l'image du joueur
     screen.blit(game.player.image, game.player.rect)
 
     #verifier si on va gauche ou droite:
-    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x < 870:
+    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x < 950:
         game.player.move_right()
-    if game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 195:
+    if game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
         game.player.move_left()
 
     if game.pressed.get(pygame.K_SPACE):
