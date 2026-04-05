bd4678a52c5537a36dfc4af0f6a2996e0bbddc9f
PaulineLandry <pauline.landry@efrei.net>
2026-04-05 12:45:39 +0200

    added main_menu and buttons files to create the starting menu

---
+++ main_menu.py
@@ -0,0 +1,47 @@
+
+
+
+def main_menu():
+    running = True
+    while running:
+        SCREEN.blit(BG_menu, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(80).render("Trash Cat 67", True, "#11369E")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250),text_input="PLAY", font=get_font(75), base_color="#d9fcd4", hovering_color="White")
+        CREDITS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400),text_input="CREDITS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550),text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, CREDITS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    running = False
+                    #play()
+                    pygame.quit()
+                if CREDITS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    running = False
+                    #credits()
+                    pygame.quit()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    running = False
+                    pygame.quit()
+
+
+        pygame.display.update()
+
+
+main_menu()
