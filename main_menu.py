64215d95c3d24910c71e853f209e8cbe0d6efe02
PaulineLandry <pauline.landry@efrei.net>
2026-04-07 14:39:25 +0200

    added the font for the menu

--- main_menu.py
+++ main_menu.py
@@ -1,21 +1,28 @@
 import pygame
 from buttons import Button
-from main import screen, background_img
+#from main import screen, background_img
 
+pygame.init()
+def get_font(size):
+    return pygame.font.Font("asset/font.ttf", size)
+
+screen = pygame.display.set_mode((1080,720))
+
+background = pygame.image.load("asset/bg_brouillon.jpg")
 
 def main_menu():
     running = True
     while running:
-        screen.blit(background_img, (0, 0))
+        screen.blit(background, (0, 0))
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
         MENU_TEXT = get_font(80).render("Trash Cat 67", True, "#11369E")
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250),text_input="PLAY", font=get_font(75), base_color="#d9fcd4", hovering_color="White")
-        CREDITS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400),text_input="CREDITS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550),text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        PLAY_BUTTON = Button(image=None, pos=(640, 250),text_input="PLAY", font=get_font(75), base_color="#d9fcd4", hovering_color="White")
+        CREDITS_BUTTON = Button(image=None, pos=(640, 400),text_input="CREDITS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=None, pos=(640, 550),text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
 
         screen.blit(MENU_TEXT, MENU_RECT)
 
