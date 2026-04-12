054d684122ccd70f5c117d0ca97ee9a853753efd
Emi-Marin <emeline.marin@efrei.net>
2026-04-12 22:21:21 +0200

    1: sfx and sound

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import main_menu
 
 def main_loop():
     pygame.init()
+    pygame.mixer.init()
     pygame.display.set_caption("Trash Cat 67")
     # The display mode is set here so main_menu can use it.
     # main_menu will call pygame.display.set_mode again with the same size — that's fine.
