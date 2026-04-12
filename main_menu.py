46771c4a3d18d7e8320127a87cb6294a06662b15
Emi-Marin <emeline.marin@efrei.net>
2026-04-12 23:03:58 +0200

    1: credit

--- main_menu.py
+++ main_menu.py
@@ -64,13 +64,83 @@ def main_menu():
 
                 SCREEN.fill("white")
 
-                CREDITS_TEXT = get_font(20).render("TO DO", True, "Black")
-                CREDITS_RECT = CREDITS_TEXT.get_rect(center=(280, 100))
-                SCREEN.blit(CREDITS_TEXT, CREDITS_RECT)
+                CREDITS_TITLE = get_font(50).render("Credits", True, "Black")
+                CREDITS_TITLE_RECT = CREDITS_TITLE.get_rect(center=(540, 50))
+                SCREEN.blit(CREDITS_TITLE, CREDITS_TITLE_RECT)
+
+                CREATORS_TITLE = get_font(30).render("Creators", True, "Blue")
+                CREATORS_TITLE_RECT = CREATORS_TITLE.get_rect(center=(540, 120))
+                SCREEN.blit(CREATORS_TITLE, CREATORS_TITLE_RECT)
+
+                CREATORS_SUB = get_font(20).render("This game was designed and built by:", True, "Black")
+                CREATORS_SUB_RECT = CREATORS_SUB.get_rect(center=(540, 155))
+                SCREEN.blit(CREATORS_SUB, CREATORS_SUB_RECT)
+
+                CREATORS_NAMES = get_font(20).render("Pauline  Emeline  Kavani  Alaa  Yann  Leonard  Fatoumata", True, "Black")
+                CREATORS_NAMES_RECT = CREATORS_NAMES.get_rect(center=(540, 183))
+                SCREEN.blit(CREATORS_NAMES, CREATORS_NAMES_RECT)
+
+                THANKS_TITLE = get_font(30).render("Special Thanks", True, "Blue")
+                THANKS_TITLE_RECT = THANKS_TITLE.get_rect(center=(540, 230))
+                SCREEN.blit(THANKS_TITLE, THANKS_TITLE_RECT)
+
+                THANKS_1 = get_font(20).render("A big thanks to all little guys who answered :", True, "Black")
+                THANKS_1_RECT = THANKS_1.get_rect(center=(540, 265))
+                SCREEN.blit(THANKS_1, THANKS_1_RECT)
+
+                THANKS_2 = get_font(20).render("'what kind of game you want to play ?'", True, "Black")
+                THANKS_2_RECT = THANKS_2.get_rect(center=(540, 290))
+                SCREEN.blit(THANKS_2, THANKS_2_RECT)
+
+                THANKS_3 = get_font(20).render("they all said the same thing:", True, "Black")
+                THANKS_3_RECT = THANKS_3.get_rect(center=(540, 315))
+                SCREEN.blit(THANKS_3, THANKS_3_RECT)
+
+                THANKS_4 = get_font(20).render("A game with brainrot.", True, "Red")
+                THANKS_4_RECT = THANKS_4.get_rect(center=(540, 340))
+                SCREEN.blit(THANKS_4, THANKS_4_RECT)
+
+                THANKS_5 = get_font(20).render("Challenge accepted. We hope we delivered.", True, "Black")
+                THANKS_5_RECT = THANKS_5.get_rect(center=(540, 365))
+                SCREEN.blit(THANKS_5, THANKS_5_RECT)
+
+                EFREI_TITLE = get_font(30).render("EFREI", True, "Blue")
+                EFREI_TITLE_RECT = EFREI_TITLE.get_rect(center=(540, 410))
+                SCREEN.blit(EFREI_TITLE, EFREI_TITLE_RECT)
+
+                EFREI_1 = get_font(20).render("We would like to express our gratitude to EFREI", True, "Black")
+                EFREI_1_RECT = EFREI_1.get_rect(center=(540, 443))
+                SCREEN.blit(EFREI_1, EFREI_1_RECT)
+
+                EFREI_2 = get_font(20).render("for providing us with the tools, resources, and support", True, "Black")
+                EFREI_2_RECT = EFREI_2.get_rect(center=(540, 468))
+                SCREEN.blit(EFREI_2, EFREI_2_RECT)
+
+                EFREI_3 = get_font(20).render("that made this project possible.", True, "Black")
+                EFREI_3_RECT = EFREI_3.get_rect(center=(540, 493))
+                SCREEN.blit(EFREI_3, EFREI_3_RECT)
 
                 CREDITS_BACK = Button(image=None, pos=(640, 460), text_input="BACK", font=get_font(75),
                                       base_color="Black", hovering_color="Green")
 
+                COPYRIGHT_TITLE = get_font(30).render("Copyright", True, "Blue")
+                COPYRIGHT_TITLE_RECT = COPYRIGHT_TITLE.get_rect(center=(540, 538))
+                SCREEN.blit(COPYRIGHT_TITLE, COPYRIGHT_TITLE_RECT)
+
+                COPYRIGHT_1 = get_font(20).render("Toby Fox for the song 'School'", True, "Black")
+                COPYRIGHT_1_RECT = COPYRIGHT_1.get_rect(center=(540, 571))
+                SCREEN.blit(COPYRIGHT_1, COPYRIGHT_1_RECT)
+
+                COPYRIGHT_2 = get_font(20).render("8-bit universe for the song 'Take On Me'",True, "Black")
+                COPYRIGHT_2_RECT = COPYRIGHT_2.get_rect(center=(540, 596))
+                SCREEN.blit(COPYRIGHT_2, COPYRIGHT_2_RECT)
+
+                COPYRIGHT_3 = get_font(20).render("Pac-man for SFX", True, "Black")
+                COPYRIGHT_3_RECT = COPYRIGHT_3.get_rect(center=(540, 621))
+                SCREEN.blit(COPYRIGHT_3, COPYRIGHT_3_RECT)
+
+                CREDITS_BACK = Button(image=None, pos=(540, 670), text_input="BACK", font=get_font(40),
+                                      base_color="Black", hovering_color="Green")
                 CREDITS_BACK.changeColor(CREDITS_MOUSE_POS)
                 CREDITS_BACK.update(SCREEN)
 
